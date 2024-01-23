9c1c098082eee056f85d62ee964097b58168b85b
Ifedayojosh <adeleyeifedayojoshua@gmail.com>
2024-01-23 12:55:05 +0100

    this is my second commit

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ function validateform() {
        
     if(firstname == "") {  
         document.getElementById("message1").innerHTML = "First Name cannot be empty";
+        document.getElementById("ec1a").style.display = "inline";
       } 
       if(lastname == "") {  
         document.getElementById("message2").innerHTML = "Last Name cannot be empty";
+        document.getElementById("ec2a").style.display = "inline";
       } 
       if(email == "") {  
         document.getElementById("message3").innerHTML = "Looks like this is not an email";
+        document.getElementById("ec3a").style.display = "inline";
       }
       if(password == "") {  
         document.getElementById("message4").innerHTML = "Password cannot be empty";
+        document.getElementById("ec4a").style.display = "inline";
       }  
          else {
         alert ("Your free trial has started!");
@@ -29,4 +33,8 @@ function removeErrorMessage(){
     document.getElementById("message2").innerHTML= "";
     document.getElementById("message3").innerHTML= "";
     document.getElementById("message4").innerHTML= "";
+    document.getElementById("ec1a").style.display = "none";
+    document.getElementById("ec2a").style.display = "none";
+    document.getElementById("ec3a").style.display = "none";
+    document.getElementById("ec4a").style.display = "none";
     }
